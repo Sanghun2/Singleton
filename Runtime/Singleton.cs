2f9be8db208a998a60da5b7cb7f8ec1591bfa8be
lshzang86 <lshzang86@naver.com>
2026-03-10 00:46:12 +0900

    Singleton Init

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace BilliotGames
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null) {
+                    _instance = FindAnyObjectByType<T>();
+                }
+
+                return _instance;
+            }
+        }
+
+        private static T _instance;
+
+        protected virtual void Awake() {
+            if (_instance == null) {
+                _instance = GetComponent<T>();
+            }
+            else {
+                var @this = GetComponent<T>();
+                if (_instance.Equals(@this) == false) {
+                    Destroy(this);
+                }
+            }
+        }
+    }
+}
